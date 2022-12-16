ca0440c47a1ae2cdc6e5b591da15a3e4dde38dbc
Roy Hyunjin Han <rhh@crosscompute.com>
2022-12-16 04:23:17 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
 
 setup(
     name='invisibleroads-macros-web',
-    version='0.2.1',
+    version='0.2.2',
     description='Shortcut functions for web operations',
     long_description=DESCRIPTION,
     long_description_content_type='text/markdown',
